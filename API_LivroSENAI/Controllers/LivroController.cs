582d0d66e8c7de00966d5a10d5d82ad6f21622a1
Tiago de Almeida <tiago.engenheiro.petroleo@gmail.com>
2022-08-27 18:10:43 -0300

    UC14

--- API_LivroSENAI/Controllers/LivroController.cs
+++ API_LivroSENAI/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using API_LivroSENAI.Repositories;
+﻿using API_LivroSENAI.Models;
+using API_LivroSENAI.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +24,77 @@ namespace API_LivroSENAI.Controllers
             try
             {
                 return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+               Livro LivroBuscado =  _livroRepository.BuscarPorId(id);
+
+                if (LivroBuscado == null)
+                {
+                    return NotFound();
+                }
+                return Ok(LivroBuscado);    
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(Livro n)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(n);  
+
+                return StatusCode(201); 
+                
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+
+                return Ok("Livro removido");
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id")]
+        public IActionResult Alterar(int id, Livro n)
+        {
+            try
+            {
+              _livroRepository.Alterar(id, n);
+
+                return StatusCode(204);
             }
             catch (Exception e)
             {
--- API_LivroSENAI/Repositories/LivroRepository.cs
+++ API_LivroSENAI/Repositories/LivroRepository.cs
@@ -17,5 +17,42 @@ namespace API_LivroSENAI.Repositories
             return _context.Livros.ToList();
         }
 
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+        public void Cadastrar(Livro n)
+        {
+            _context.Livros.Add(n);
+
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Livro n = _context.Livros.Find(id);
+
+            _context.Livros.Remove(n);
+
+            _context.SaveChanges();
+        }
+
+        public void Alterar(int id, Livro n)
+        {
+            Livro livroBuscado = _context.Livros.Find(id);
+
+            if (livroBuscado != null)
+            {
+                livroBuscado.Titulo = n.Titulo;
+                livroBuscado.QuantidadedePaginas = n.QuantidadedePaginas;
+                livroBuscado.Disponivel = n.Disponivel;
+
+                _context.Livros.Update(livroBuscado);
+
+                _context.SaveChanges();
+            }
+        }
+
     }
 }
